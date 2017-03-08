d4b0f525c017263b0c30da61e77701d9a5e9ac5e
Devan Walker <devanrwalker@gmail.com>
2017-03-08 11:34:35 -0700

    walker tool initial commit

---
+++ lib/walker_view_tool.rb
@@ -0,0 +1,6 @@
+require "walker_view_tool/version"
+require "walker_view_tool/renderer"
+
+module WalkerViewTool
+  # Your code goes here...
+end
---
+++ lib/walker_view_tool/version.rb
@@ -0,0 +1,3 @@
+module WalkerViewTool
+  VERSION = "0.1.0"
+end
